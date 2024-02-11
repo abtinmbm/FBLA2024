17c6b9434e738a2f4528f3e4c262065ddd4a19a2
abtinmbm <abtintest@gmail.com>
2024-02-10 21:07:44 -0500

    Added Terms and Conditions to urls.py

--- partners/urls.py
+++ partners/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import partner_list, partner_proposal, partner_list_json
+from .views import partner_list, partner_proposal, partner_list_json, partner_terms
 
 urlpatterns = [
     # URL pattern for rendering partner list page
@@ -8,4 +8,6 @@ urlpatterns = [
     path('json/partner_list', partner_list_json, name='partner_list_json'),
     # URL pattern for handling partner proposal form submission
     path('partner', partner_proposal, name='partner_proposal'),
+    # URL pattern for terms and conditions page
+    path('terms', partner_terms, name='terms'),
 ]
